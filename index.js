b88831db24e6a8ed160983bcca147f739cf9f7bc
Metaladmiral <thesecretinspiration@gmail.com>
2024-03-16 02:41:09 +0530

    Added prisma, typescript

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const {login} = require('./src/controllers')
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express()
+app.use(express.urlencoded({ 
+  extended: true
+})) 
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/login', (req, res) => {
-  login(req, res)
+app.post('/login', (req, res) => {
+  login(req, res, process.env.TOKEN_SECRET)
 })
 
 app.listen(port, () => {
--- src/auth/login.js
+++ src/auth/login.js
@@ -1,6 +1,18 @@
-
-
-function login(req, res) {
-    res.send('login api')
+const jwt = require('jsonwebtoken');
+function login(req, res, TOKEN_SECRET) {
+    
+    const {username, pass} = req.body
+    if(!username || !pass) {
+        res.status(404).json({})
+        return
+    }    
+    
+    res.send(username+" "+pass)
+    // function generateAccessToken(userData) {
+    //     return jwt.sign(userData, TOKEN_SECRET, { expiresIn: '2629800s' });
+    // }
+    // let jwtToken = generateAccessToken()
+    // res.send(`JWT TOKEN: ${jwtToken}`)
+    
 }
 module.exports = login
