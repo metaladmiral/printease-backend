4f430213bee475b1f70d9483e6f791798e802bbb
Metaladmiral <thesecretinspiration@gmail.com>
2024-03-21 02:50:23 +0530

    Feat: added Register API

--- src/auth/register.ts
+++ src/auth/register.ts
@@ -1,19 +1,39 @@
-import { Request, Response } from "express";
-function register(req: Request, res: Response) {
-    const {username, pass} = req.body
+import { Request, Response } from "express"
+import { PrismaClient } from '@prisma/client'
+import { v4 as uuidv4 } from 'uuid';
+import crypto from 'crypto';
 
-    if(!username || !pass) {
+const prisma = new PrismaClient()
+
+async function register(req: Request, res: Response) {
+    const {username, pass, email, phone} = req.body
+
+    if(!username || !pass || !email || !phone) {
         res.status(404).json({})
         return
     }    
-    res.send(username+" "+pass)
+
+    const userId = uuidv4()
+    const hashedPass = crypto.createHash('sha256').update(pass).digest('hex');
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+            username: username,
+            email: email,
+            phone: phone,
+            pass: hashedPass,
+            user_id: userId,
+            user_type: "1"
+            },
+        })
+        res.status(201).json({ user });
+    }
+    catch(err) {
+        res.status(500).json({ error: 'Failed to register user' });
+    }
     
 
-    // function generateAccessToken(userData) {
-    //     return jwt.sign(userData, TOKEN_SECRET, { expiresIn: '2629800s' });
-    // }
-    // let jwtToken = generateAccessToken()
-    // res.send(`JWT TOKEN: ${jwtToken}`)
     
 }
 
