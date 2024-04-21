590df1a8b8ef3ff40b9f6fbc10cd56aeacd9c0f6
Metaladmiral <thesecretinspiration@gmail.com>
2024-04-21 17:05:24 +0530

    Fix: createOrder file name

--- src/controller.ts
+++ src/controller.ts
@@ -1,6 +1,6 @@
 import login from "./auth/login"
 import register from "./auth/register"
-import createOrder from "./user/create-order"
+import createOrder from "./user/createOrder"
 import getMyOrders from "./user/getMyOrders"
 import getOrderDetails from "./user/getOrderDetails"
 import getPendingOrders from "./owner/getPendingOrders"
