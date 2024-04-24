3ad7966bc71e86fa64ea6212d894b3bd9ff2d2a7
Metaladmiral <thesecretinspiration@gmail.com>
2024-04-24 17:41:57 +0530

    Formatted code for getoRDERDetails

--- src/user/getOrderDetails.ts
+++ src/user/getOrderDetails.ts
@@ -1,47 +1,42 @@
 import { Response } from "express";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 import { RequestWithUser } from "../types";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function getOrderDetails(req: RequestWithUser, res: Response) {
+  if (req.user === undefined) {
+    res.status(403).send("Access Forbidden");
+    return;
+  }
 
-    if(req.user===undefined) {
-        res.status(403).send("Access Forbidden")
-        return;
-    }
+  let order_id = req.query.order_id;
 
-    let order_id = req.query.order_id
+  order_id = order_id?.toString();
 
-    order_id = order_id?.toString()
+  try {
+    let orderDetails = await prisma.order.findUnique({
+      where: {
+        order_id: order_id,
+      },
+      include: {
+        OrderDetails: {
+          select: {
+            file_details: true,
+            page_size: true,
+            print_color: true,
+            print_type: true,
+            total_pages: true,
+            price_per_page: true,
+          },
+        },
+      },
+    });
 
-
-    try {
-        let orderDetails = await prisma.order.findUnique({
-            where: {
-                order_id: order_id
-            },
-            include: {
-                OrderDetails: {
-                    select: {
-                        file_details: true,
-                        page_size: true,
-                        print_color: true,
-                        print_type: true,
-                        total_pages: true,
-                        price_per_page: true
-                    }
-                }
-            }
-        })
-    
-        res.send(orderDetails)
-    }
-    catch(err) {
-        res.send("DB error")
-    }
-
-
+    res.send(orderDetails);
+  } catch (err) {
+    res.send("DB error");
+  }
 }
 
-export default getOrderDetails
+export default getOrderDetails;
