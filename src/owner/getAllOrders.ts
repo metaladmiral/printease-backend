c66479cd8aca6ac45bef1abd81ef21cde9f0d461
Metaladmiral <thesecretinspiration@gmail.com>
2024-04-25 22:43:08 +0530

    Fix: initialization of let, offset in getAllOrders, and formatting fix

--- src/owner/getAllOrders.ts
+++ src/owner/getAllOrders.ts
@@ -1,39 +1,30 @@
 import { Response } from "express";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 import { RequestWithUser } from "../types";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function getAllOrders(req: RequestWithUser, res: Response) {
-    let offset, limit
-    if(req.body.limit == undefined) {
-        limit = 10
-    }
-    if(req.body.offset == undefined) {
-        offset = 0
-    }
+  if (req.user === undefined) {
+    res.status(403).send("Access Forbidden");
+    return;
+  }
 
-    if(req.user===undefined) {
-        res.status(403).send("Access Forbidden")
-        return;
-    }
+  let limit = req.body.limit !== undefined ? parseInt(req.body.limit) : 10;
+  let offset = req.body.offset !== undefined ? parseInt(req.body.offset) : 0;
 
-    try {
-
-        let orderDetails = await prisma.order.findMany({
-            take: limit,
-            skip: offset,
-            orderBy: {
-                updatedAt: 'desc'
-            }
-        })
-        res.send(orderDetails)
-
-    }catch(err) {
-        res.send("DB ERROR")
-    }
-
-
+  try {
+    let orderDetails = await prisma.order.findMany({
+      take: limit,
+      skip: offset,
+      orderBy: {
+        updatedAt: "desc",
+      },
+    });
+    res.send(orderDetails);
+  } catch (err) {
+    res.send("DB ERROR");
+  }
 }
 
-export default getAllOrders
+export default getAllOrders;
