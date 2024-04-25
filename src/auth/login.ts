fa1119cc70cbf2efddd751f4be31ad4470584dd7
Metaladmiral <thesecretinspiration@gmail.com>
2024-04-25 12:51:16 +0530

    Fix: added user_type in login resp

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -1,49 +1,47 @@
 import { Request, Response } from "express";
-import crypto from 'crypto';
-import jwt, { Secret } from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
+import crypto from "crypto";
+import jwt, { Secret } from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-async function chkCredsFromDb(email:string, hashedPass:string) {
-    let user = await prisma.user.findUnique({
-        where: {
-            email: email,
-            pass: hashedPass,
-        },
-    })
-    return user
+async function chkCredsFromDb(email: string, hashedPass: string) {
+  let user = await prisma.user.findUnique({
+    where: {
+      email: email,
+      pass: hashedPass,
+    },
+  });
+  return user;
 }
 
-async function login(req: Request, res: Response, TOKEN_SECRET:Secret) {
-    const {email, pass} = req.body
+async function login(req: Request, res: Response, TOKEN_SECRET: Secret) {
+  const { email, pass } = req.body;
 
-    if(!email || !pass) {
-        res.status(404).json({})
-        return
-    }    
-    const hashedPass = crypto.createHash('sha256').update(pass).digest('hex');
+  if (!email || !pass) {
+    res.status(404).json({});
+    return;
+  }
+  const hashedPass = crypto.createHash("sha256").update(pass).digest("hex");
 
-    let user;
-    try {
-        user = await chkCredsFromDb(email, hashedPass)
-    }
-    catch(err) {
-        res.send("DB error: "+err)
-        return
-    }
+  let user;
+  try {
+    user = await chkCredsFromDb(email, hashedPass);
+  } catch (err) {
+    res.send("DB error: " + err);
+    return;
+  }
 
-    if(!user) {
-        res.send({"user_found": "0"})
-        return
-    }
-    
-    function generateAccessToken(userData: Object) {
-        return jwt.sign(userData, TOKEN_SECRET, { expiresIn: '2629800s' });
-    }
-    let jwtToken = generateAccessToken(user)
-    res.send({"user_found": "1", "JWT_TOKEN": jwtToken})
+  if (!user) {
+    res.send({ user_found: "0" });
+    return;
+  }
 
+  function generateAccessToken(userData: Object) {
+    return jwt.sign(userData, TOKEN_SECRET, { expiresIn: "2629800s" });
+  }
+  let jwtToken = generateAccessToken(user);
+  res.send({ user_found: "1", JWT_TOKEN: jwtToken, user_type: user.user_type });
 }
 
-export default login
+export default login;
