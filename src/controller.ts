ff75e0d921caac01420d314b5177856c2fb33f2e
Metaladmiral <thesecretinspiration@gmail.com>
2024-05-01 23:41:20 +0530

    Fix: moves public apis files to new /src/public dir

--- src/controller.ts
+++ src/controller.ts
@@ -1,12 +1,12 @@
 import login from "./auth/login";
 import register from "./auth/register";
 import createOrder from "./user/createOrder";
-import getOrderDetails from "./common/getOrderDetails";
+import getOrderDetails from "./public/getOrderDetails";
 import getOrders from "./common/getOrders";
 import getAllOrders from "./common/getAllOrders";
 import updateOrderStatus from "./owner/updateOrderStatus";
-import getUserDetails from "./common/getUserDetails";
-import getPerPagePrice from "./common/getPerPagePrice";
+import getUserDetails from "./public/getUserDetails";
+import getPerPagePrice from "./public/getPerPagePrice";
 import sendEmail from "./mail/sendEmail";
 import updateOrderPaymentId from "./user/updateOrderPaymentId";
 
